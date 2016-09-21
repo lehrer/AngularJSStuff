e994a3de785c658ffe9ced891bf869d926c05d4d
Gershon Lehrer <gershonlehrer@Gershons-MacBook-Pro.local>
2016-09-21 18:53:01 +0200

    first angular js dir commit

---
+++ OpenDataApiViaAngularJS-1/controllerOpenData.js
@@ -0,0 +1,41 @@
+var app = angular.module('myApp', []); 
+		app.controller('openDataCtrl', function($scope, $http, $interval) {
+			$scope.url = "https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json";
+			$scope.url2 = "https://datatank.stad.gent/4/mobiliteit/bezettingparkeergaragesnmbs.json"
+			$scope.ToggleButtonDetailsGent = false;
+			$scope.getData = function() {
+			$http.get($scope.url).then(
+					function (response) {
+						$scope.items = response.data; 
+					}); 
+					$http.get($scope.url2).then(
+					function (response) {
+						$scope.items2 = response.data; 
+					});
+			}; 
+			
+		
+			$scope.secondsToWait = 10;
+	$scope.timeCounter = $scope.secondsToWait;		
+$interval(function () {
+	if ($scope.timeCounter == 0) {
+		$scope.timeCounter = $scope.secondsToWait;
+$scope.getData();
+	}
+$scope.timeCounter -=1;
+}, ($scope.secondsToWait * 100 + 1)); 
+//als secondsToWait op 0 staat, zal bovenstaande lus blijkbaar slechts 1 keer worden uitgevoerd, vandaar +1
+
+			
+			$scope.getTrColor = function (value) {
+if (value == 0) {
+return 'red'; 
+}
+else if (value < 100) {
+return 'orange'; 
+	  }
+else {
+	return '#7FFF00'; 
+}
+}; 
+		}); 
